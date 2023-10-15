fb4c516cafcdd57a8170b159e70768f34db20dfa
JuanMaCarrearas <manuucarreras@gmail.com>
2023-10-14 22:05:58 -0300

    done

--- M03_variablesydatos/Prep_Course_Homework_03.py
+++ M03_variablesydatos/Prep_Course_Homework_03.py
@@ -7,6 +7,9 @@
 
 # In[7]:
 
+var = 399
+print(var)
+
 
 
 
@@ -14,7 +17,7 @@
 
 # In[3]:
 
-
+type(8.5)
 
 
 
@@ -22,7 +25,7 @@
 
 # In[8]:
 
-
+type(var)
 
 
 
@@ -30,13 +33,14 @@
 
 # In[2]:
 
-
+mi_nombre = 'solid snake'
 
 
 # 5) Crear una variable que contenga un número complejo
 
 # In[3]:
 
+var_n_complejo = 90 + 6j
 
 
 
@@ -46,6 +50,7 @@
 # In[4]:
 
 
+type(var_n_complejo)
 
 
 
@@ -63,13 +68,17 @@ pi = 3.1416
 
 
 
+# No son lo mismo, var2 (True) representa un valor booleano y var1 ('True') representa un String (cadena de carracteres)
 
+var1 = 'True'
+var2 = True
 
 # 9) Imprimir el tipo de dato correspondientes a las variables creadas en el punto 8
 
 # In[5]:
 
 
+print('var1 es de tipo ', type(var1), ' y var2 es de tipo ' , type(var2))
 
 
 
@@ -77,7 +86,7 @@ pi = 3.1416
 
 # In[1]:
 
-
+suma = 23 + 3.5
 
 
 
@@ -85,23 +94,29 @@ pi = 3.1416
 
 # In[2]:
 
+var1 = 5j
+var2 = 3j
 
-
+print(var1 + var2)
 
 
 # 12) Realizar una operación de suma de un número real y otro complejo
 
 # In[4]:
 
+var1 = 2j
+var2 = 1.34
 
-
+print(var1 + var2)
 
 
 # 13) Realizar una operación de multiplicación
 
 # In[5]:
 
+var = 5 * 5
 
+print(var1)
 
 
 
@@ -109,6 +124,9 @@ pi = 3.1416
 
 # In[6]:
 
+var = 2 ** 8
+
+print(var)
 
 
 
@@ -117,6 +135,9 @@ pi = 3.1416
 # In[8]:
 
 
+var  = 27 / 4 
+print(var)
+
 
 
 
@@ -124,7 +145,8 @@ pi = 3.1416
 
 # In[9]:
 
-
+var = 27 // 4
+print(var)
 
 
 
@@ -133,13 +155,16 @@ pi = 3.1416
 # In[1]:
 
 
-
+var = 27 % 4
+print(var)
 
 
 # 18) Utilizando como operandos el número 4 y los resultados obtenidos en los puntos 16 y 17. Obtener 27 como resultado
 
 # In[2]:
 
+var = 6 * 4 + 3
+print(var)
 
 
 
@@ -148,16 +173,19 @@ pi = 3.1416
 
 # In[3]:
 
+var1 = 'Que'
+var2 = ' Onda'
 
-
+print(var1 + var2)
 
 
 # 20) Evaluar si "2" es igual a 2. ¿Por qué ocurre eso?
 
 # In[4]:
 
-
-
+var1 = '2'
+var2 = 2
+print(var1 == var2)
 
 
 # 21) Utilizar las funciones de cambio de tipo de dato, para que la validación del punto 20 resulte verdadera
@@ -165,6 +193,9 @@ pi = 3.1416
 # In[11]:
 
 
+var1 = '2'
+var2 = 2
+print(int(var1) == var2)
 
 
 
@@ -180,6 +211,10 @@ pi = 3.1416
 
 # In[15]:
 
+var = 3
+var -= 1
+
+print(var)
 
 
 
